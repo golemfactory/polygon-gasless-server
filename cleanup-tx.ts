6ef39bbe89883f1385216f5a1150e9eb392c23b7
Przemyslaw Rekucki <prekucki@rcl.pl>
2022-04-21 13:34:33 +0200

    + added cleanup-tx

---
+++ cleanup-tx.ts
@@ -0,0 +1,39 @@
+import web3, { config } from './config.ts';
+import { utils } from './sci.ts';
+
+const { secret } = config;
+const addedAccount = web3.eth.accounts.wallet.add(secret!);
+const gasPrice = web3.utils.toHex(web3.utils.toWei('70', 'gwei'));
+const gasLimit = web3.utils.toHex(800 * 1000);
+const [nonce, pendingNonce] = await Promise.all([
+    web3.eth.getTransactionCount(addedAccount.address, 'latest'),
+    web3.eth.getTransactionCount(addedAccount.address, 'pending'),
+]);
+
+if (nonce !== pendingNonce) {
+    console.log('nonce=', nonce, 'pending=', pendingNonce);
+    const customChain = {
+        name: 'Polygon',
+        chainId: 137,
+        networkId: 137,
+    };
+
+    const txObject = {
+        nonce: nonce,
+        gasLimit,
+        // Keep gas price hardcoded, prevents from spending too much in case if chain is under artificial heavy load
+        gasPrice,
+        // Address inserted in the wallet
+        from: addedAccount.address,
+        to: addedAccount.address,
+        common: {
+            customChain,
+        },
+    };
+
+    await web3.eth.sendTransaction(txObject)
+        .on('transactionHash', (hash_returned: string) => console.log('tx=', hash_returned));
+}
+else {
+    console.warn("nonce==pendingNonce", nonce, pendingNonce);
+}
